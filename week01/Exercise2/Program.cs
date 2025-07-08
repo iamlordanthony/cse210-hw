9da1d28494383a48aed9d388d9cd07a2594f3b0a
iamlordanthony <lordsenan@gmail.com>
2025-07-08 16:51:20 +0000

    Program Updated

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,65 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        // Prompt the user for their grade percentage
+        Console.Write("What is your grade percentage? ");
+        string input = Console.ReadLine();
+        int percentage = int.Parse(input);
+
+        string letter = "";
+        string sign = "";
+
+        // Determine the letter grade
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // Determine pass/fail
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass the course. Keep working hard and you'll get it next time!");
+        }
+
+        // Stretch challenge: determine "+" or "-" sign
+        int lastDigit = percentage % 10;
+
+        if (letter != "A" && letter != "F") // no A+ or F+/-
+        {
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+        }
+        else if (letter == "A" && percentage < 93)
+        {
+            sign = "-";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}{sign}");
     }
 }
