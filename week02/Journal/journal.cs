6a22c833fb4960fce080cc33376e709b3ff2e60f
iamlordanthony <lordsenan@gmail.com>
2025-07-12 18:09:00 +0000

    Week02 Updated

---
+++ week02/Journal/journal.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Journal
+{
+    private List<Entry> _entries = new List<Entry>();
+
+    public void AddEntry(Entry newEntry)
+    {
+        _entries.Add(newEntry);
+    }
+
+    public void DisplayAll()
+    {
+        foreach (Entry entry in _entries)
+        {
+            entry.Display();
+            Console.WriteLine();
+        }
+    }
+
+    public void SaveToFile(string filename)
+    {
+        using (StreamWriter outputFile = new StreamWriter(filename))
+        {
+            foreach (Entry entry in _entries)
+            {
+                outputFile.WriteLine($"{entry._date}|{entry._prompt}|{entry._response}");
+            }
+        }
+        Console.WriteLine($"Journal saved to {filename}");
+    }
+
+    public void LoadFromFile(string filename)
+    {
+        _entries.Clear();
+        string[] lines = File.ReadAllLines(filename);
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split('|');
+            if (parts.Length == 3)
+            {
+                Entry entry = new Entry(parts[0], parts[1], parts[2]);
+                _entries.Add(entry);
+            }
+        }
+        Console.WriteLine($"Journal loaded from {filename}");
+    }
+}
